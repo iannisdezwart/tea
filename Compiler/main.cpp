b4892a8bf3667eb1ab8414d18faa215d230f8b0d
iannisdezwart <iannis__@hotmail.com>
2021-01-26 13:56:21 +0100

    🛠 Fixed Argument Indices

--- Compiler/main.cpp
+++ Compiler/main.cpp
@@ -9,8 +9,8 @@ int main(int argc, char **argv)
 {
 	if (argc < 2) err("Usage: ./compile input.tea output");
 
-	char *input_file_name = argv[0];
-	char *output_file_name = argv[1];
+	char *input_file_name = argv[1];
+	char *output_file_name = argv[2];
 
 	compile(input_file_name, output_file_name);
 }
