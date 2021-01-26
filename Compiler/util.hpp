e6ac325c57ebea058852cb854fe95857fcd62b24
iannisdezwart <iannis__@hotmail.com>
2021-01-26 13:40:37 +0100

    🔤 Added Alphanumericality Checking Macros

--- Compiler/util.hpp
+++ Compiler/util.hpp
@@ -9,6 +9,12 @@
 	exit(0); \
 } while (0)
 
+#define is_alpha(c) (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' \
+	|| c == '$' || c == '_')
+
+#define is_alphanumeric(c) (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' \
+	|| c == '$' || c == '_' || c >= '0' && c <= '9')
+
 using namespace std;
 
 #endif
