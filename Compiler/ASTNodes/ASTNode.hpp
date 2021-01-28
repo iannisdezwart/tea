f5fdc2a967466f8c89ced54aa6c5c42b43333843
iannisdezwart <iannis__@hotmail.com>
2021-01-28 11:33:12 +0100

    🚀 Added New ASTNodes & Created Handy Abstract Methods

--- Compiler/ASTNodes/ASTNode.hpp
+++ Compiler/ASTNodes/ASTNode.hpp
@@ -9,12 +9,24 @@ enum ASTNodeType {
 	TYPE_IDENTIFIER_PAIR,
 	FUNCTION_DECLARATION,
 	RETURN_STATEMENT,
-	CODE_BLOCK
+	CODE_BLOCK,
+	LITERAL_STRING_EXPRESSION,
+	LITERAL_CHAR_EXPRESSION,
+	LITERAL_NUMBER_EXPRESSION,
+	IDENTIFIER_EXPRESSION
 };
 
 class ASTNode {
 	public:
 		ASTNodeType type;
+
+		virtual string to_str() = 0;
+		virtual void dfs(function<void(ASTNode *, size_t)>, size_t depth = 0) = 0;
+
+		void print(const char *prefix)
+		{
+			printf("%s: %s\n", prefix, to_str().c_str());
+		}
 };
 
 #endif
