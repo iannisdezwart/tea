a4cbbdf7ccbb9959f4498bf5eed31088cb2e68a1
iannisdezwart <iannis__@hotmail.com>
2021-01-28 11:35:23 +0100

    🛠 Compiler Now Calls The Parser

--- Compiler/compiler.hpp
+++ Compiler/compiler.hpp
@@ -4,6 +4,8 @@
 #include <bits/stdc++.h>
 
 #include "util.hpp"
+#include "tokeniser.hpp"
+#include "parser.hpp"
 
 using namespace std;
 
@@ -18,7 +20,22 @@ void compile(char *input_file_name, char *output_file_name)
 	if (output_file == NULL)
 		err("Output file %s does not exist", output_file_name);
 
-	vector<>
+	Tokeniser tokeniser(input_file);
+	vector<Token> tokens = tokeniser.tokenise();
+
+	printf("Number of tokens: %ld\n\n", tokens.size());
+
+	for (Token token : tokens) {
+		printf("Token: { type: %d, value: \"%s\", line: %ld, col: %ld }\n",
+			token.type, token.value.c_str(), token.line, token.col);
+	}
+
+	Parser parser(tokens);
+	parser.parse();
+	parser.print_ast();
+
+	fclose(input_file);
+	fclose(output_file);
 }
 
 #endif
