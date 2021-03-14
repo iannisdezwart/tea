87a82272472f84093a282798eff1529ab116fb9a
iannisdezwart <iannis__@hotmail.com>
2021-03-14 16:07:39 +0100

    🚀 Added RAM Device

---
+++ VM/ram-device.hpp
@@ -0,0 +1,56 @@
+#ifndef TEA_RAM_DEVICE_HEADER
+#define TEA_RAM_DEVICE_HEADER
+
+#include <bits/stdc++.h>
+
+#include "memory-device.hpp"
+#include "../Assembler/buffer.hpp"
+
+using namespace std;
+
+class RamDevice : public MemoryDevice, public Buffer {
+	public:
+		RamDevice(uint64_t from, uint64_t to)
+			: MemoryDevice(from, to),
+				Buffer(Buffer::alloc(to - from)) {}
+
+		void copy_from(uint8_t *source, size_t source_size)
+		{
+			memcpy(data, source, source_size);
+		}
+
+		uint64_t size()
+		{
+			return to - from;
+		}
+
+		template <typename intx_t>
+		intx_t get(uint64_t offset)
+		{
+			return *((intx_t *) (data + offset));
+		}
+
+		template <typename intx_t>
+		void set(uint64_t offset, intx_t value)
+		{
+			*((intx_t *) (data + offset)) = value;
+		}
+
+		void dump(
+			uint64_t left_bound,
+			uint64_t right_bound,
+			uint64_t highlight_fg = -1,
+			uint64_t highlight_bg = -1
+		) {
+			for (uint64_t i = left_bound; i < right_bound; i++) {
+				if (highlight_fg == i) printf("\x1b[31m");
+				if (highlight_bg == i) printf("\x1b[43m");
+				uint8_t byte = get<uint8_t>(i - from);
+				printf("0x%04lx    0x%02hhx    %03hhu\x1b[m\n", i, byte, byte);
+			}
+
+			printf("\n");
+		}
+};
+
+#endif
