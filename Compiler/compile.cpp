1f314a1998c2f0f22c11798f7cd29882bef2e895
iannisdezwart <iannis__@hotmail.com>
2021-06-01 09:32:36 +0200

    🧹 Renamed Source File

--- Compiler/compile.cpp
+++ Compiler/compile.cpp
@@ -10,13 +10,15 @@ using namespace std;
 int main(int argc, char **argv)
 {
 	if (argc < 2) {
-		fprintf(stderr, "Usage: ./compile input.tea output.teax\n");
+		fprintf(stderr, "Usage: ./compile input.tea output.teax [ --debug ]\n");
 		exit(1);
 	}
 
 	char *input_file_name = argv[1];
 	char *output_file_name = argv[2];
+	string debug_flag = argc > 3 ? argv[3] : "";
+	bool debug = debug_flag == "--debug" || debug_flag == "-d";
 
-	Compiler compiler(input_file_name, output_file_name);
+	Compiler compiler(input_file_name, output_file_name, debug_flag == "--debug");
 	compiler.compile();
 }
