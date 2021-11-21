a641e645c1bd29b12c3966a2484d1065a9bfbd93
iannisdezwart <iannis__@hotmail.com>
2021-11-21 13:41:59 +0100

    📖 Wrote Documentation For The Disassembler

--- Disassembler/disassembler.hpp
+++ Disassembler/disassembler.hpp
@@ -10,27 +10,49 @@
 
 using namespace std;
 
+/**
+ * @brief Class that is responsible for disassembling a bytecode file.
+ */
 class Disassembler {
 	public:
+		// The file reader for the bytecode file.
 		FileReader file_reader;
+
+		// The output file.
 		FILE *file_out;
+
+		// Flag that indicates whether an argument is the first
+		// or not. Used to determine whether to print a comma or not.
 		bool first_arg;
 
+		/**
+		 * @brief Constructs a new Disassembler object.
+		 * Initialises the file reader and the output file.
+		 * @param file_in The input file.
+		 * @param file_out The output file.
+		 */
 		Disassembler(FILE *file_in, FILE *file_out)
 			: file_reader(file_in), file_out(file_out), first_arg(true) {}
 
+		/**
+		 * @brief Pretty-prints an instruction to the output file.
+		 * @param instruction The instruction to be printed.
+		 */
 		void print_instruction(const char *instruction)
 		{
-			// Print address in green
+			// Print address in green.
 
 			fprintf(file_out, ANSI_GREEN "0x" ANSI_BRIGHT_GREEN "%04lx" ANSI_RESET "    ",
 				file_reader.read_bytes - 18);
 
-			// Print instruction in orange
+			// Print instruction in orange.
 
 			fprintf(file_out, ANSI_YELLOW "%-33s" ANSI_RESET, instruction);
 		}
 
+		/**
+		 * @brief Pretty-prints an argument to the output file.
+		 */
 		void print_arg()
 		{
 			if (first_arg) {
@@ -41,42 +63,65 @@ class Disassembler {
 			}
 		}
 
+		/**
+		 * @brief Pretty-prints a register argument to the output file.
+		 * @param reg_id The register id.
+		 */
 		void print_arg_reg(uint8_t reg_id)
 		{
 			print_arg();
 			fprintf(file_out, ANSI_CYAN "%s" ANSI_RESET, CPU::reg_to_str(reg_id));
 		}
 
+		/**
+		 * @brief Pretty-prints a literal argument to the output file.
+		 * @param num The literal number to print.
+		 */
 		void print_arg_literal_number(uint64_t num)
 		{
 			print_arg();
 			fprintf(file_out, ANSI_YELLOW "%ld" ANSI_RESET, num);
 		}
 
+		/**
+		 * @brief Pretty-prints an address argument to the output file.
+		 * @param str The address to print.
+		 */
 		void print_arg_address(uint64_t address)
 		{
 			print_arg();
 			fprintf(file_out, ANSI_GREEN "0x" ANSI_BRIGHT_GREEN "%lx" ANSI_RESET, address);
 		}
 
+		/**
+		 * @brief Prints a string comment argument to the output file.
+		 * @param str The string to print.
+		 */
 		void print_arg_null_terminated_string(char *str)
 		{
 			print_arg();
 			fprintf(file_out, ANSI_BRIGHT_BLACK "/* %s */", str);
 		}
 
+		/**
+		 * @brief Ends the current instruction.
+		 */
 		void end_args()
 		{
 			fprintf(file_out, "\n");
 			first_arg = true;
 		}
 
+		/**
+		 * @brief Disassembles the bytecode file.
+		 * The instructions are printed to the output file.
+		 */
 		void disassemble()
 		{
 			uint64_t static_data_size = file_reader.read<uint64_t>();
 			uint64_t program_size = file_reader.read<uint64_t>();
 
-			// Print static data
+			// Print static data.
 
 			fprintf(file_out, "Static data (size = %lu)\n\n", static_data_size);
 
@@ -87,7 +132,7 @@ class Disassembler {
 					i, byte, byte, byte);
 			}
 
-			// Print program
+			// Print the program.
 
 			fprintf(file_out, "\nProgram (size = %lu)\n\n", program_size);
 
@@ -101,6 +146,8 @@ class Disassembler {
 				vector<ArgumentType> args = instruction_arg_types(instruction);
 
 				print_instruction(instruction_str);
+
+				// Print all arguments.
 
 				for (ArgumentType arg : args) {
 					switch (arg) {
--- Disassembler/file-reader.hpp
+++ Disassembler/file-reader.hpp
@@ -5,19 +5,34 @@
 
 using namespace std;
 
+/**
+ * @brief Class that is used to read a file in chunks of user-defined size.
+ */
 class FileReader {
 	public:
+		// The file to read from.
 		FILE *file;
+
+		// The number of read bytes so far.
 		size_t read_bytes = 0;
 
+		/**
+		 * @brief Constructs a new File Reader object.
+		 * @param file The file to read from.
+		 */
 		FileReader(FILE *file) : file(file) {}
 
+		/**
+		 * @brief Reads a chunk of data from the file.
+		 * @tparam intx_t The type of the data to read.
+		 * @returns The read chunk.
+		 */
 		template <typename intx_t>
 		intx_t read()
 		{
 			intx_t value = 0;
 
-			// Little endian
+			// Only works on little-endian systems.
 
 			for (uint8_t i = 0; i < sizeof(intx_t); i++) {
 				int c = fgetc(file);
