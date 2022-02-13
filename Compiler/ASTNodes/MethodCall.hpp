9c52cad697b207f7a1931eaea12b8bd4f565acb4
iannisdezwart <iannis__@hotmail.com>
2022-02-13 13:57:45 +0100

    🧹 Formatted `MethodCall.hpp`

--- Compiler/ASTNodes/MethodCall.hpp
+++ Compiler/ASTNodes/MethodCall.hpp
@@ -13,153 +13,176 @@
 #include "../compiler-state.hpp"
 #include "../tokeniser.hpp"
 
-struct MethodCall : public ReadValue {
-		IdentifierExpression *object;
-		FunctionCall *method;
-		Token op_token;
-		enum Operator op;
+struct MethodCall : public ReadValue
+{
+	IdentifierExpression *object;
+	FunctionCall *method;
+	Token op_token;
+	enum Operator op;
 
-		MethodCall(IdentifierExpression *object, FunctionCall *method,
-			const Token& op_token)
-				: object(object), method(method), op_token(op_token),
-					ReadValue(op_token, METHOD_CALL) {}
+	MethodCall(IdentifierExpression *object, FunctionCall *method,
+		const Token &op_token)
+		: object(object), method(method), op_token(op_token),
+		  ReadValue(op_token, METHOD_CALL) {}
 
-		void dfs(std::function<void(ASTNode *, size_t)> callback, size_t depth)
+	void
+	dfs(std::function<void(ASTNode *, size_t)> callback, size_t depth)
+	{
+		object->dfs(callback, depth + 1);
+		method->dfs(callback, depth + 1);
+
+		callback(this, depth);
+	}
+
+	std::string
+	to_str()
+	{
+		std::string s = "MethodCall { callee = \"";
+		s += object->identifier_token.value;
+		s += "::" + method->get_name() + "\" } @ ";
+		s += to_hex((size_t) this);
+		return s;
+	}
+
+	std::string
+	get_object_class_name(CompilerState &compiler_state)
+	{
+		const std::string &obj_name = object->identifier_token.value;
+		Type type                   = compiler_state.get_type_of_identifier(obj_name);
+
+		if (type != Type::USER_DEFINED_CLASS)
 		{
-			object->dfs(callback, depth + 1);
-			method->dfs(callback, depth + 1);
-
-			callback(this, depth);
+			err_at_token(object->accountable_token, "Type Error",
+				"Cannot access methods from %s of type %s\n"
+				"Methods are only accessible from classes",
+				obj_name.c_str(), type.to_str().c_str());
 		}
 
-		std::string to_str()
+		return type.class_name;
+	}
+
+	Type
+	get_type(CompilerState &compiler_state)
+	{
+		const std::string &class_name  = get_object_class_name(compiler_state);
+		const Class &cl                = compiler_state.classes[class_name];
+		const std::string &method_name = method->get_name();
+
+		if (!cl.has_method(method_name))
 		{
-			std::string s = "MethodCall { callee = \"" + object->identifier_token.value
-				+ "::" + method->get_name() + "\" } @ " + to_hex((size_t) this);
-			return s;
+			err_at_token(method->accountable_token, "Call to undeclared method",
+				"Method %s::%s was called, but not declared",
+				class_name.c_str(), method_name.c_str());
 		}
 
-		std::string get_object_class_name(CompilerState& compiler_state)
+		return cl.get_method(method_name).id.type;
+	}
+
+	void
+	get_value(Assembler &assembler, CompilerState &compiler_state, uint8_t res_reg)
+	{
+		uint8_t arg_reg;
+
+		std::string fn_name = get_object_class_name(compiler_state) + "::" + method->get_name();
+
+		if (!compiler_state.functions.count(fn_name))
 		{
-			const std::string& obj_name = object->identifier_token.value;
-			Type type = compiler_state.get_type_of_identifier(obj_name);
-
-			if (type != Type::USER_DEFINED_CLASS) {
-				err_at_token(object->accountable_token, "Type Error",
-					"Cannot access methods from %s of type %s\n"
-					"Methods are only accessible from classes",
-					obj_name.c_str(), type.to_str().c_str());
-			}
-
-			return type.class_name;
+			err_at_token(method->accountable_token, "Call to undeclared method",
+				"Method %s was called, but not declared",
+				fn_name.c_str());
 		}
 
-		Type get_type(CompilerState& compiler_state)
+		Function &fn                    = compiler_state.functions[fn_name];
+		std::vector<Identifier> &params = fn.parameters;
+
+		// Add the this pointer to the argument list
+
+		Token address_of_token;
+		address_of_token.type  = OPERATOR;
+		address_of_token.value = "&";
+
+		IdentifierExpression *obj_id = new IdentifierExpression(*object);
+		UnaryOperation *this_pointer = new UnaryOperation(obj_id, address_of_token, true);
+		method->arguments.insert(method->arguments.begin(), this_pointer);
+
+		// Validate arguments
+
+		if (method->arguments.size() != params.size())
 		{
-			const std::string& class_name = get_object_class_name(compiler_state);
-			const Class& cl = compiler_state.classes[class_name];
-			const std::string& method_name = method->get_name();
-
-			if (!cl.has_method(method_name))
-				err_at_token(method->accountable_token, "Call to undeclared method",
-					"Method %s::%s was called, but not declared",
-					class_name.c_str(), method_name.c_str());
-
-			return cl.get_method(method_name).id.type;
+			err_at_token(method->fn_token,
+				"Type Error",
+				"Argument count does not equal parameter count"
+				"Method %s expects %ld arguments, got %ld",
+				fn_name.c_str(), params.size(), method->arguments.size());
 		}
 
-		void get_value(Assembler& assembler, CompilerState& compiler_state, uint8_t res_reg)
+		size_t args_size = 0;
+
+		if (method->arguments.size())
 		{
-			uint8_t arg_reg;
+			arg_reg = assembler.get_register();
+		}
 
-			std::string fn_name = get_object_class_name(compiler_state) + "::" + method->get_name();
+		for (size_t i = 0; i < method->arguments.size(); i++)
+		{
+			Type &param_type = params[i].type;
+			Type arg_type    = method->arguments[i]->get_type(compiler_state);
 
-			if (!compiler_state.functions.count(fn_name))
-				err_at_token(method->accountable_token, "Call to undeclared method",
-					"Method %s was called, but not declared",
-					fn_name.c_str());
+			std::string param = param_type.to_str();
+			std::string arg   = arg_type.to_str();
 
-			Function& fn = compiler_state.functions[fn_name];
-			std::vector<Identifier>& params = fn.parameters;
-
-			// Add the this pointer to the argument list
-
-			Token address_of_token;
-			address_of_token.type = OPERATOR;
-			address_of_token.value = "&";
-
-			IdentifierExpression *obj_id = new IdentifierExpression(*object);
-			UnaryOperation *this_pointer = new UnaryOperation(obj_id, address_of_token, true);
-			method->arguments.insert(method->arguments.begin(), this_pointer);
-
-			// Validate arguments
-
-			if (method->arguments.size() != params.size())
+			if (param_type != arg_type)
 				err_at_token(method->fn_token,
 					"Type Error",
-					"Argument count does not equal parameter count"
-					"Method %s expects %ld arguments, got %ld",
-					fn_name.c_str(), params.size(), method->arguments.size());
+					"Method call arguments list does not match "
+					"method parameter type template\n"
+					"argument[%lu] is of type %s. Expected type %s",
+					i, arg.c_str(), param.c_str());
 
-			size_t args_size = 0;
+			size_t byte_size = param_type.byte_size();
 
-			if (method->arguments.size()) arg_reg = assembler.get_register();
+			// Stores result into the argument register
 
-			for (size_t i = 0; i < method->arguments.size(); i++) {
-				Type& param_type = params[i].type;
-				Type arg_type = method->arguments[i]->get_type(compiler_state);
+			method->arguments[i]->get_value(assembler, compiler_state, arg_reg);
 
-				std::string param = param_type.to_str();
-				std::string arg = arg_type.to_str();
+			switch (byte_size)
+			{
+			case 1:
+				assembler.push_reg_8(arg_reg);
+				break;
 
-				if (param_type != arg_type)
-					err_at_token(method->fn_token,
-						"Type Error",
-						"Method call arguments list does not match "
-						"method parameter type template\n"
-						"argument[%lu] is of type %s. Expected type %s",
-						i, arg.c_str(), param.c_str());
+			case 2:
+				assembler.push_reg_16(arg_reg);
+				break;
 
-				size_t byte_size = param_type.byte_size();
+			case 4:
+				assembler.push_reg_32(arg_reg);
+				break;
 
-				// Stores result into the argument register
+			case 8:
+				assembler.push_reg_64(arg_reg);
+				break;
 
-				method->arguments[i]->get_value(assembler, compiler_state, arg_reg);
-
-				switch (byte_size) {
-					case 1:
-						assembler.push_reg_8(arg_reg);
-						break;
-
-					case 2:
-						assembler.push_reg_16(arg_reg);
-						break;
-
-					case 4:
-						assembler.push_reg_32(arg_reg);
-						break;
-
-					case 8:
-						assembler.push_reg_64(arg_reg);
-						break;
-
-					default:
-						err_at_token(method->fn_token,
-							"Type Error",
-							"Function call argument does not fit in a register\n"
-							"Behaviour is not implemented yet\n"
-							"argument[%lu] is of type %s (width = %lu)",
-							i, arg.c_str(), arg_type.byte_size());
-				}
-
-				args_size += byte_size;
+			default:
+				err_at_token(method->fn_token,
+					"Type Error",
+					"Function call argument does not fit in a register\n"
+					"Behaviour is not implemented yet\n"
+					"argument[%lu] is of type %s (width = %lu)",
+					i, arg.c_str(), arg_type.byte_size());
 			}
 
-			if (method->arguments.size()) assembler.free_register(arg_reg);
-
-			assembler.push_64(args_size);
-			assembler.call(fn_name);
+			args_size += byte_size;
 		}
+
+		if (method->arguments.size())
+		{
+			assembler.free_register(arg_reg);
+		}
+
+		assembler.push_64(args_size);
+		assembler.call(fn_name);
+	}
 };
 
 #endif
